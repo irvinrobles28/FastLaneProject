8caaea04bb216365d00f30bf88ac4cfa905a0f8e
Irvin Robles <irvin.robles@globant.com>
2017-07-18 12:30:44 -0500

    First project

---
+++ FastLaneProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastLaneProject
+//
+//  Created by Irvin Robles Soriano on 7/18/17.
+//  Copyright © 2017 Globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+project 'FastLaneProject.xcodeproj'
+use_frameworks!
+target 'FastLaneProject' do
+pod 'Alamofire'
+end
